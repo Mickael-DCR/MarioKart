4d5057ba45db1dfe0020f89dca5abc5e48bcf66a
Onipote <m.dacostareis@rns.ecolescreatives.com>
2025-02-27 19:53:28 +0100

    clear scripts

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemBox : MonoBehaviour
+public class Items : ScriptableObject
 {
-            
     
 }
