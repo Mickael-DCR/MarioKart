3c6b82ff4581c11595d88517ee91a0690ce7917c
MickaelD <m.dacostareis@rns.ecolescreatives.com>
2025-03-14 17:00:38 +0100

    Laps

--- Assets/Scripts/LapTracker.cs
+++ Assets/Scripts/LapTracker.cs
@@ -2,23 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LapTracker : MonoBehaviour
 {
-    [SerializeField] private int _flags, _totalFlags, _laps, _totalLaps;
+    [SerializeField] private int _totalFlags, _laps, _totalLaps;
+    [SerializeField] private List<GameObject> _flagsList;
+    [SerializeField] private TextMeshProUGUI _lapsText;
+
+    private void Start()
+    {
+        _lapsText.text = _laps+"/"+_totalLaps;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Flag"))
         {
-            _flags++;
-            if (_flags >= _totalFlags)
+            _flagsList.Add(other.gameObject);
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.CompareTag("Finish"))
+        {
+            
+            if (_flagsList.Count == _totalFlags)
             {
-                _flags = 0;
-                _laps++;
+                for (int i = 0; i < _flagsList.Count; i++)
+                {
+                    _flagsList[i].SetActive(true);
+                }
+                
+                if (_laps >= _totalLaps)
+                {
+                    Debug.Log("Race Complete!");
+                }
+                else
+                {
+                    _flagsList.Clear();
+                    _lapsText.text = ++_laps+"/"+_totalLaps;
+                }
             }
         }
     }
-    
+
     
 }
